6327fbd70e3322f70f0b8357ba021e8b09a62b5e
jasonlcook <tarquinnlcook@gmail.com>
2022-02-13 15:04:59 +0000

    Add interupts and debugs

--- matrix_clock/matrix_clock.ino
+++ matrix_clock/matrix_clock.ino
@@ -1,11 +1,19 @@
 #include <Wire.h>
 #include <DS3231.h>
-
 #include <MaxMatrix.h>
 
 //todo:
 //  -   add alarm buzzer
-//  -   add update via bluetooth
+//  -   add tamper proofing
+//  -   add interupt buttons for setting
+//  -   add OLED for real output
+//  -   user RTC alarm for countdown to allow for poweroutage
+//  -   set one minute mode to only use 60 LEDs
+//  -   replace delays with clock reads
+
+const byte debug = true;
+
+const byte PIN_LED = 13;
 
 //Time
 RTClib myRTC;
@@ -18,10 +26,10 @@ uint32_t secondDuration;
 uint32_t secondsTick;
 
 //LED Matix
-int DIN = 7; // DIN pin of MAX7219 module
-int CLK = 6; // CLK pin of MAX7219 module
-int CS = 5;  // CS pin of MAX7219 module
-int maxInUse = 1;
+const byte DIN = 7; // DIN pin of MAX7219 module
+const byte CLK = 6; // CLK pin of MAX7219 module
+const byte CS = 5;  // CS pin of MAX7219 module
+const byte maxInUse = 1;
 
 MaxMatrix m(DIN, CS, CLK, maxInUse);
 
@@ -30,8 +38,22 @@ uint32_t currentColumn;
 int32_t currentRow;
 bool cursorState = true;
 
+//Buttons
+const byte PIN_SET_INTERRUPT = 2;
+
+const byte PIN_SET_UP = 9;
+const byte PIN_SET_DOWN = 10;
+
+volatile byte settingTimer = false;
+
+volatile byte countdown = true;
+volatile byte timeset = false;
+
 void setup()
 {
+    if (debug)
+        Serial.println("setup()");
+
     //Time init
     Serial.begin(9600);
     Wire.begin();
@@ -40,23 +62,63 @@ void setup()
     m.init();
     m.setIntensity(0); // initial led matrix intensity, 0-15
 
+    //buttons
+    pinMode(PIN_LED, OUTPUT);
+    pinMode(PIN_SET_INTERRUPT, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(PIN_SET_INTERRUPT), setTimerInterrupt, RISING);
+
     //Initialise
-    //set default duration
-    calculateDuration("1");
+    calculateDuration("5");
+}
+
+void setTimerInterrupt()
+{
+    if (!settingTimer)
+        settingTimer = true;
+    else
+        settingTimer = false;
 }
 
 void loop()
 {
+    if (debug)
+        Serial.println("mainLoop()");
+
+    if (settingTimer)
+        setTimer();
+    else
+        countDown();
+
+    delay(100);
+}
+
+void setTimer()
+{
+    if (debug)
+        Serial.println("setTimer()");
+
+    calculateDuration("1");
+
+    settingTimer = false;
+}
+
+void countDown()
+{
+    if (debug)
+        Serial.println("countDown()");
+
+    //Read serial value
     while (Serial.available())
     {
         minutesDuration = Serial.readString();
         calculateDuration(minutesDuration);
     }
 
+    //Tick seconds
     DateTime now = myRTC.now();
     currentTime = now.unixtime();
 
-    //tick seconds
     if (currentTime != indexTime)
     {
         secondsSinceLastUpdate++;
@@ -75,11 +137,19 @@ void loop()
         secondsSinceLastUpdate = 0;
     }
 
+    //Flash cursor
     flashCursor();
 }
 
 void calculateDuration(String input)
 {
+    if (debug)
+    {
+        Serial.print("calculateDuration(");
+        Serial.print(input);
+        Serial.println(")");
+    }
+
     uint32_t parsedInput = input.toInt();
 
     secondDuration = parsedInput * 60;
@@ -96,6 +166,9 @@ void calculateDuration(String input)
 
 void reset()
 {
+    if (debug)
+        Serial.println("reset()");
+
     currentRow = 7;
     currentColumn = 0;
 
@@ -111,6 +184,9 @@ void reset()
 
 void fill()
 {
+    if (debug)
+        Serial.println("fill()");
+
     m.clear();
 
     for (int i = 0; i < 8; i++)
@@ -121,6 +197,13 @@ void fill()
 
 uint32_t calculateTick(int duration)
 {
+    if (debug)
+    {
+        Serial.print("calculateTick(");
+        Serial.print(duration);
+        Serial.println(")");
+    }
+
     double tick = (double)duration / (double)lightsInMatrix;
     uint32_t flooredTick = round(tick);
 
@@ -138,6 +221,9 @@ uint32_t calculateTick(int duration)
 
 void updateMatrix()
 {
+    if (debug)
+        Serial.println("updateMatrix()");
+
     currentRow -= 1;
     if (currentRow < 0)
     {
@@ -145,9 +231,7 @@ void updateMatrix()
         currentColumn += 1;
         if (currentColumn > 7)
         {
-            fill();
-            currentRow = 7;
-            currentColumn = 0;
+            alarm();
         }
     }
 
@@ -160,8 +244,25 @@ void updateMatrix()
 
 void flashCursor()
 {
+    if (debug)
+        Serial.println("flashCursor()");
+
     m.setDot(currentColumn, currentRow, true);
     delay(250);
     m.setDot(currentColumn, currentRow, false);
     delay(250);
+}
+
+void alarm()
+{
+    if (debug)
+        Serial.println("alarm()");
+
+    while (true)
+    {
+        fill();
+        delay(250);
+        m.clear();
+        delay(250);
+    }
 }
